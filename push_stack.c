89501c6ee1c8a47def656c20a6516d6ecff240e7
shazaaly <shaza.aly@gmail.com>
2023-06-22 19:34:22 +0300

    bett 2

--- push_stack.c
+++ push_stack.c
@@ -1,24 +1,27 @@
 #include "monty.h"
-
+/**
+* push_stack - Pushes a new element onto the stack.
+*
+* @top: A pointer to the top of the stack.
+* @value: The value to be pushed onto the stack.
+*/
 void push_stack(stack_t **top, int value)
 {
-    stack_t *new_node;
+	stack_t *new_node;
 
-    new_node = malloc(sizeof(stack_t));
-    if (new_node == NULL)
-    {
-        fprintf(stderr, "Error: malloc failed\n");
-        exit(EXIT_FAILURE);
-    }
-
-    new_node->n = value;
-    new_node->prev = NULL;
-
-    if (*top != NULL) /* stack not empty */
-    {
-        new_node->next = *top;
-        (*top)->prev = new_node;
-    }
-
-    *top = new_node;
+	new_node = malloc(sizeof(stack_t));
+	if (new_node == NULL)
+	{
+	    fprintf(stderr, "Error: malloc failed\n");
+	    exit(EXIT_FAILURE);
+	}
+	new_node->n = value;
+	new_node->prev = NULL;
+	if (*top != NULL) /* stack not empty */
+	{
+	    new_node->next = *top;
+	    (*top)->prev = new_node;
+	}
+	*top = new_node;
 }
+
