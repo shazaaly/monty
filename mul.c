97626023e109bb360abc95e5aa70149a8116bc6b
shazaaly <shaza.aly@gmail.com>
2023-06-23 01:35:21 +0300

    is integer in mul

--- mul.c
+++ mul.c
@@ -16,6 +16,13 @@ void mul(stack_t **top, unsigned int line_number)
 		free(col.line);
 		exit(EXIT_FAILURE);
 	}
+		if (!is_integer((*top)->n) || !is_integer((*top)->next->n))
+	{
+		fprintf(stderr, "L%u: usage: push integer\n", line_number);
+		fclose(col.file);
+		free(col.line);
+		exit(EXIT_FAILURE);
+	}
 	res = (*top)->n * (*top)->next->n;
 	tmp = *top;
 	*top = (*top)->next; /*new top is sec node*/
@@ -23,4 +30,17 @@ void mul(stack_t **top, unsigned int line_number)
 	(*top)->n = res;
 	free(tmp);
 
+}
+/**
+ * is_integer - Checks if a given number is an integer
+ * @num: The number to check
+ *
+ * Return: 1 if the number is an integer, 0 otherwise
+ */
+int is_integer(int num)
+{
+	if (num == (int)num)
+		return (1);
+	else
+		return (0);
 }
